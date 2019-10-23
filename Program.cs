0d3bbcd227b8cf4ccd6209e5a1d6916d43561b0c
zerot13 <imac0.13@gmail.com>
2019-10-23 15:57:17 -0700

    Implement PrintDataToFile

--- Program.cs
+++ Program.cs
@@ -10,72 +10,8 @@ namespace MtGACardDataParser
     {
         static void Main(string[] args)
         {
-            string cardDataString = File.ReadAllText("data_cards.json");
-            string cardDataKeyFile = File.ReadAllText("data_loc.json");
-            string cardEnumsFile = File.ReadAllText("data_enums.json");
-            JsonSerializer jsonSerializer = new JsonSerializer();
-            CardDataJson[] cdArray = JsonConvert.DeserializeObject<CardDataJson[]>(cardDataString);
-            List<CardDataJson> eldList = cdArray.Where(c => c.set == "ELD").ToList();
-
-            DataKeySetJson dataKeySet = JsonConvert.DeserializeObject<DataKeySetJson>(cardDataKeyFile);
-            TypeEnumJson[] typeEnumSet = JsonConvert.DeserializeObject<TypeEnumJson[]>(cardEnumsFile);
-
-            List<CardDataFull> fullEldList = eldList.ConvertAll(j => new CardDataFull { 
-                mtgaId = j.grpid,
-                isCollectible = j.isCollectible,
-                CollectorNumber = j.CollectorNumber,
-                set = j.set,
-                types = convertTypes(j.types, typeEnumSet.First(t => t.name == "CardType").values, dataKeySet.keys),
-                subtypes = convertTypes(j.subtypes, typeEnumSet.First(t => t.name == "SubType").values, dataKeySet.keys),
-                name = convertName(j.titleId, dataKeySet.keys),
-                colors = convertColors(j.colors, typeEnumSet.First(t => t.name == "Color").values, dataKeySet.keys),
-                colorIdentity = convertColorIdentity(j.colorIdentity, typeEnumSet.First(t => t.name == "Color").values, dataKeySet.keys),
-                castingcost = CostData.ConvertCost(j.castingcost),
-                rarity = RarityData.ConvertRarity(j.rarity)
-             });
-        }
-
-        private static string convertColors(string[] colors, DataKeyJson[] values, DataKeyJson[] dataKey)
-        {
-            if(colors.Length == 0)
-            {
-                return "['Colorless']";
-            }
-            List<string> enumTypeList = colors.ToList().ConvertAll(t => values.First(v => v.id == t).text);
-            string convertedColors = "['" + string.Join("', '", enumTypeList.ConvertAll(e => convertName(e, dataKey))) + "']";
-
-            return convertedColors;
-        }
-
-        private static string convertColorIdentity(string[] colorId, DataKeyJson[] values, DataKeyJson[] dataKey)
-        {
-            if(colorId.Length == 0)
-            {
-                return "[]";
-            }
-            List<string> enumTypeList = colorId.ToList().ConvertAll(t => values.First(v => v.id == t).text);
-            string convertedColors = "['" + string.Join("', '", enumTypeList.ConvertAll(e => convertAndShortenColor(e, dataKey))) + "']";
-
-            return convertedColors;
-        }
-
-        private static string convertAndShortenColor(string colorId, DataKeyJson[] dataKey)
-        {
-            string fullColor = dataKey.First(k => k.id == colorId).text;
-            return ColorData.ShortenColorName(fullColor);
-        }
-
-        private static string convertTypes(string[] types, DataKeyJson[] values, DataKeyJson[] dataKey)
-        {
-            List<string> enumTypeList = types.ToList().ConvertAll(t => values.First(v => v.id == t).text);
-            string convertedType = string.Join(" ", enumTypeList.ConvertAll(e => convertName(e, dataKey)));
-
-            return convertedType;
-        }
-
-        private static string convertName(string titleId, DataKeyJson[] dataKey)
-        {
-            return dataKey.First(k => k.id == titleId).text;
+            List<CardDataFull> fullEldList = ParseCardData.ParseCards();
+            PrintData.PrintDataToFile(fullEldList);
         }
     }
 }
